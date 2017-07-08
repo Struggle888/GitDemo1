2eb3f9198efafdb00ae41b311a154599f8e45e01
lpf <136447572@qq.com>
2017-07-08 11:46:56 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/taisheng/gitdemo1/zc.java
@@ -0,0 +1,30 @@
+package com.taisheng.gitdemo1;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/7/8.
+ */
+
+public class zc {
+    public void feature1(View view) {
+        if ("".equals("功能1")) {
+        } else {
+        }
+    }
+    public void feature2(View view) {
+        if ("".equals("功能1")) {
+        } else {
+        }
+    }
+    public void feature3(View view) {
+        if ("".equals("功能1")) {
+        } else {
+        }
+    }
+    public void feature4(View view) {
+        if ("".equals("功能1")) {
+        } else {
+        }
+    }
+}
